6e3cbf796d06f1746a9c82db00199df8e4bc8239
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-08-28 18:56:01 -0300

    Adiciona atividade de Ananda
    Atividade da Semana 2: Alert soma, multiplicação e lista de compras.

---
+++ atividade-ananda/tarefinha.js
@@ -0,0 +1,28 @@
+alert('Seja bem-vindo ao meu site.')
+
+{
+    let num87 = 87
+    let num96 = 96
+    let soma = num87+num96
+    alert('87 + 96 = ' + soma)
+
+}
+
+{
+
+let num5 = 5
+let num8 = 8
+let multipilicacao = num5*num8
+alert('5 x 8 = ' + multipilicacao)
+
+}
+
+{
+
+let compras = ['Água sanitária', ' Baygon', ' leite', ' cuscuz', ' arroz', ' ovo', ' jerimum.']
+alert('Lista de compras: ' + compras)
+
+
+}
+
+
